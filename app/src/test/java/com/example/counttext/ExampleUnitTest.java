e18995cebf031f670823124a81805eb6d47185c1
linuzas <100625146+linuzas@users.noreply.github.com>
2022-04-01 16:16:58 +0300

    Test Done

--- app/src/test/java/com/example/counttext/ExampleUnitTest.java
+++ app/src/test/java/com/example/counttext/ExampleUnitTest.java
@@ -1,17 +1,98 @@
 package com.example.counttext;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import utils.TextUtils;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void getCharsCount_Given_SymbolString_Result_Six() {
+        String givenString = "??????";
+        int expectedValue = 6;
+        int actualValue = TextUtils.getCharsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
     }
+
+    @Test
+    public void getCharsCount_Given_SpacesString_Result_Three() {
+        String givenString = "   ";
+        int expectedValue = 3;
+        int actualValue = TextUtils.getCharsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getCharsCount_Given_EmptyString_Result_Zero() {
+        String givenString = "";
+        int expectedValue = 0;
+        int actualValue = TextUtils.getCharsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getCharsCount_Given_NumberString_Result_Five() {
+        String givenString = "02468";
+        int expectedValue = 5;
+        int actualValue = TextUtils.getCharsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getCharsCount_Given_NormalString_Result_Sixteen() {
+        String givenString = "Testing the code";
+        int expectedValue = 16;
+        int actualValue = TextUtils.getCharsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+
+    @Test (expected = NullPointerException.class)
+    public void getCharsCount_Given_NullString_Result_Exception() {
+        String givenString = null;
+        int actualValue = TextUtils.getCharsCount(givenString);
+    }
+
+    @Test
+    public void getWordsCount_Given_EmptyString_Result_Zero() {
+        String givenString = "";
+        int expectedValue = 0;
+        int actualValue = TextUtils.getWordsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getWordsCount_Given_SpacesString_Result_Eight() {
+        String givenString = "        ";
+        int expectedValue = 0;
+        int actualValue = TextUtils.getWordsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getWordsCount_Given_NumberString_Result_one() {
+        String givenString = "999777";
+        int expectedValue = 1;
+        int actualValue = TextUtils.getWordsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getWordsCount_Given_NormalString_Result_Sixteen() {
+        String givenString = "Testing the code";
+        int expectedValue = 2;
+        int actualValue = TextUtils.getWordsCount(givenString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+
+
+
 }
